dc7652b26c116b4320727eece25d66d0c148aea9
stargazer <stargazer>
2024-12-07 09:49:20 +0700

    Add ProjectAkhirASD folder and Java files

---
+++ Monitor.java
@@ -0,0 +1,19 @@
+package ProjectAkhirASD;
+
+public class Monitor {
+    String nama;
+    double harga;
+    int ukuranLayar;
+
+    public Monitor(String nama, double harga, int ukuranLayar){
+        this.nama = nama;
+        this.harga = harga;
+        this.ukuranLayar = ukuranLayar;
+    }
+
+    // Method untuk cetak
+    @Override
+    public String toString() {
+        return nama + ", Harga: USD" + harga + ", Ukuran Layar: " + ukuranLayar + " inch";
+    }
+}
